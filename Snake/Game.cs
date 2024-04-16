588194d7c39e88498db2e5bca43f63852faadc3b
Jake Godsall <jake.edward.godsall@gmail.com>
2024-04-16 09:55:48 +0100

    refactored the Game class

--- Snake/Game.cs
+++ Snake/Game.cs
@@ -1,52 +1,83 @@
+using System;
 using System.Diagnostics;
 using System.Threading;
+using System.Threading.Tasks;
 
-namespace Snake;
-
-public class Game
+namespace Snake
 {
-    public Board Board { get; set; }
-    public Snake Snake { get; set; }
-
-    public int Score { get; set; }
-    private Stopwatch stopwatch = new Stopwatch();
-
-    public static string currentDirection { get; set; } = "right";
-    private static readonly object lockObject = new object();
-    
-    public void Run()
+    public class Game
     {
-        InitializeGame(40, 20, 20, 10, 6);
-    }
+        private Board Board { get; set; }
+        private Snake Snake { get; set; }
 
-    public void InitializeGame(
-        int boardWidth,
-        int boardHeight,
-        int initialSnakeX,
-        int initialSnakeY,
-        int initialSnakeLength
+        private Food _food;
+
+        private bool GameOver { get; set; } = false;
+
+        private int Score { get; set; }
+        private readonly Stopwatch _stopwatch = new Stopwatch();
+
+        private static string CurrentDirection { get; set; } = "right";
+        private static readonly object LockObject = new object();
+
+        public void Run()
+        {
+            _stopwatch.Start();
+            InitializeGame(40, 20, 20, 10, 6);
+            var gameDelay = 200;
+
+            while (!GameOver)
+            {
+                UpdateGame();
+                Thread.Sleep(gameDelay);
+            }
+        }
+
+        private void InitializeGame(
+            int boardWidth,
+            int boardHeight,
+            int initialSnakeX,
+            int initialSnakeY,
+            int initialSnakeLength
         )
-    {
-        stopwatch.Start();
-        Task inputTask = Task.Run(() => HandleInput());
-        
-        Board = new Board(boardWidth, boardHeight);
-        var food = new Food(boardWidth, boardHeight, 'x');
-        Snake = new Snake(initialSnakeX, initialSnakeY, initialSnakeLength, '\u2588');
+        {
+            var inputTask = Task.Run(() => HandleInput());
 
-        Board.CalculateBoardState(Snake, food);
+            Board = new Board(boardWidth, boardHeight);
+            _food = new Food(boardWidth, boardHeight, 'x');
+            Snake = new Snake(initialSnakeX, initialSnakeY, initialSnakeLength, '\u2588');
 
-        DisplayGame();
+            Board.CalculateBoardState(Snake, _food);
+            Render();
+        }
 
-        var gameOver = false;
+        private void UpdateGame()
+        {
+            Board.CalculateBoardState(Snake, _food);
+            MoveSnake();
+            HandleSnakeEatsFood();
+            HandleCollision();
+            Render();
+        }
 
-        while (!gameOver)
+        private void HandleSnakeEatsFood()
+        {
+            if (Snake.IsAtCoord(_food.XPos, _food.YPos))
+            {
+                Score++;
+                Snake.hasEaten = true;
+                _food.Respawn(Board.Width, Board.Height);
+            }
+        }
+
+        private void MoveSnake()
         {
             string directionToMove;
-            lock (lockObject)
+            lock (LockObject)
             {
-                directionToMove = currentDirection;
+                directionToMove = CurrentDirection;
             }
+
             switch (directionToMove)
             {
                 case "up":
@@ -62,75 +93,65 @@ public class Game
                     Snake.MoveLeft();
                     break;
             }
-            
-            if (Snake.IsAtCoord(food.XPos, food.YPos))
-            {
-                Score++;
-                Snake.hasEaten = true;
-                food.Respawn(boardWidth, boardHeight);
-            }
-
-            if (Snake.HasCollidedWithSelf() || Snake.HasCollidedWithBorder(boardWidth, boardHeight))
-            {
-                gameOver = true;
-                stopwatch.Stop();
-            }
-            
-            
-            Board.CalculateBoardState(Snake, food);
-            DisplayGame();
-            Thread.Sleep(200);
         }
-    }
 
-    private static void HandleInput()
-    {
-        while (true)
+        private void HandleCollision()
         {
-            if (Console.KeyAvailable)
+            if (!Snake.HasCollidedWithSelf() && !Snake.HasCollidedWithBorder(Board.Width, Board.Height)) return;
+            GameOver = true;
+            _stopwatch.Stop();
+        }
+
+        private static void HandleInput()
+        {
+            while (true)
             {
-                var key = Console.ReadKey().Key;
-                lock (lockObject)
+                if (Console.KeyAvailable)
                 {
-                    switch (key)
+                    var key = Console.ReadKey(true).Key;
+                    lock (LockObject)
                     {
-                        case ConsoleKey.W:
-                        case ConsoleKey.UpArrow:
-                            if (currentDirection != "down")
-                                currentDirection = "up";
-                            break;
-                        case ConsoleKey.D:
-                        case ConsoleKey.RightArrow:
-                            if (currentDirection != "left")
-                                currentDirection = "right";
-                            break;
-                        case ConsoleKey.S:
-                        case ConsoleKey.DownArrow:
-                            if (currentDirection != "up")
-                                currentDirection = "down";
-                            break;
-                        case ConsoleKey.A:
-                        case ConsoleKey.LeftArrow:
-                            if (currentDirection != "right")
-                                currentDirection = "left";
-                            break;
+                        switch (key)
+                        {
+                            case ConsoleKey.W:
+                            case ConsoleKey.UpArrow:
+                                if (CurrentDirection != "down")
+                                    CurrentDirection = "up";
+                                break;
+                            case ConsoleKey.D:
+                            case ConsoleKey.RightArrow:
+                                if (CurrentDirection != "left")
+                                    CurrentDirection = "right";
+                                break;
+                            case ConsoleKey.S:
+                            case ConsoleKey.DownArrow:
+                                if (CurrentDirection != "up")
+                                    CurrentDirection = "down";
+                                break;
+                            case ConsoleKey.A:
+                            case ConsoleKey.LeftArrow:
+                                if (CurrentDirection != "right")
+                                    CurrentDirection = "left";
+                                break;
+                        }
                     }
                 }
-            }
-            Thread.Sleep(50);
-        }
-    }
 
-    public void DisplayGame()
-    {
-        Console.Clear();
-        Console.WriteLine("Score: " + Score + "           Time: " + FormatElapsedTime(stopwatch.Elapsed));
-        Board.Draw();
-    }
-    
-    private static string FormatElapsedTime(TimeSpan time)
-    {
-        // Formatting elapsed time as MM:SS
-        return $"{time.Minutes + time.Hours * 60:D2}:{time.Seconds:D2}";
+                Thread.Sleep(50);
+            }
+        }
+
+        private void Render()
+        {
+            Console.Clear();
+            Console.WriteLine("Score: " + Score + "           Time: " + FormatElapsedTime(_stopwatch.Elapsed));
+            Board.Draw();
+        }
+
+        private static string FormatElapsedTime(TimeSpan time)
+        {
+            // Formatting elapsed time as MM:SS
+            return $"{time.Minutes + time.Hours * 60:D2}:{time.Seconds:D2}";
+        }
     }
 }
